a4975f6be0c315bf12aee5bed11a97b65087e9d2
wukaMM <wangyujiawuka@163.com>
2020-06-05 13:19:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -46,16 +46,21 @@ export class ImageDrop {
 	handlePaste(evt) {
 		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
 	      this.readFiles(evt.clipboardData.items, dataUrl => {
-	        const userAgent = navigator.userAgent; // 取得浏览器的userAgent字符串
-	        if (userAgent.indexOf('Firefox') > -1) {
-	          const selection = this.quill.getSelection();
-	          if (selection) {
-	          } else {
-	            setTimeout(() => this.insert(dataUrl), 0);
-	          }
-	        } else {
-	          setTimeout(() => this.insert(dataUrl), 0);
-	        }
+// 	        const userAgent = navigator.userAgent; // 取得浏览器的userAgent字符串
+// 	        if (userAgent.indexOf('Firefox') > -1) {
+// 	          const selection = this.quill.getSelection();
+// 	          if (selection) {
+// 	          } else {
+// 	            setTimeout(() => this.insert(dataUrl), 0);
+// 	          }
+// 	        } else {
+// 	          setTimeout(() => this.insert(dataUrl), 0);
+// 	        }
+		  const selection = this.quill.getSelection();
+		  if (selection) {
+		  } else {
+		    setTimeout(() => this.insert(dataUrl), 0);
+		  }
 	      });
 	    }
 	}
